56b8cc94e305bccd8e985721f45b32868768daf4
radiantly <buildingsomethingfun@gmail.com>
2021-08-15 18:44:29 +0530

    Add async task using asyncio instead of awaiting
    Also format with black

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import discord
 from urllib import request
 import logging
+import asyncio
 
 client = discord.Client()
 CategoryID = 876410471115272253
 
 
 @client.event
-async def on_ready():       #bot has logged in and has set things up 
-    print('We have logged in as {0.user}'.format(client))
+async def on_ready():  # bot has logged in and has set things up
+    print(f"We have logged in as {client.user}")
 
 
 @client.event
@@ -18,22 +19,26 @@ async def on_message(message):
 
     else:
 
-        if(message.content.startswith('!send')):
+        if message.content.startswith("!send"):
 
             messageContent = message.content
-        
+
+            tasks = []
+
             for channel in message.guild.text_channels:
-                print('Sending something!')
+                print("Sending something!")
                 print(messageContent)
-                if(channel.category.id == CategoryID):
-                    await channel.send(messageContent)
+                if channel.category.id == CategoryID:
+                    tasks.append(asyncio.create_task(channel.send(messageContent)))
 
-        
+            for task in tasks:
+                await task
 
-client.run('ODc2MzM2NjUyOTEyNTcwMzY5.YRimGw.tscmdX65C9t0jnpKdtGGe1tqT6Y')
 
-logger = logging.getLogger('discord')
+client.run("ODc2MzM2NjUyOTEyNTcwMzY5.YRimGw.tscmdX65C9t0jnpKdtGGe1tqT6Y")
+
+logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
