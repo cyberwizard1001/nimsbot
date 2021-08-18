74d7522768c29d8c4037ce910725f2100d8e4aaf
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-18 23:24:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 import logging
 import asyncio
+import keep_alive
 
 client = discord.Client()
-CategoryID = 876410471115272253
-
+CategoryID = 877541025328021584
+keep_alive.keep_alive()
 
 @client.event
 async def on_ready():  # bot has logged in and has set things up
@@ -16,18 +17,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!send"):
+    else:
 
-        attachments = [attachment.url for attachment in message.attachments]
+        if message.content.startswith("!send"):
 
-        tasks = [
-            asyncio.create_task(channel.send(f"{message.content} {' '.join(attachments)}"))
-            for channel in message.guild.text_channels
-            if channel.category.id == CategoryID
-        ]
+            messageContent = message.content
 
-        for task in tasks:
-            await task
+            action = messageContent[5:]
+
+            tasks = []
+
+            for channel in message.guild.text_channels:
+                print("Sending something!")
+                print(messageContent)
+                if channel.category.id == CategoryID:
+                    tasks.append(asyncio.create_task(channel.send(action)))
+
+            for task in tasks:
+                await task
 
 
 client.run("ODc2MzM2NjUyOTEyNTcwMzY5.YRimGw.tscmdX65C9t0jnpKdtGGe1tqT6Y")
@@ -37,3 +44,4 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
+
